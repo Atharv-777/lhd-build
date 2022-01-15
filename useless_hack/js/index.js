1ceb4129cd3af924be18f62fc3e494e5d828497b
Atharv-777 <63581644+Atharv-777@users.noreply.github.com>
2022-01-15 17:37:27 +0530

    Delete useless_hack directory

--- useless_hack/js/index.js
+++
@@ -1,7 +0,0 @@
-let form = document.getElementById("form");
-let heading = document.getElementById('heading');
-let text = document.getElementById('text')
-
-function display(){
-    heading.innerHTML = "Hello, " + text.value;
-}
